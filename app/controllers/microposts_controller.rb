9923238953d5b2df287c3df4219efc724fd123bb
Arvind Subramanian <arvind@kappboom.com>
2011-08-01 15:27:12 -0700

    Added associations between micropost and user and deleted some useless lines from microposts_controller.rb.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,10 +4,10 @@ class MicropostsController < ApplicationController
   def index
     @microposts = Micropost.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @microposts }
-    end
+    # respond_to do |format|
+    #   format.html # index.html.erb
+    #   format.xml  { render :xml => @microposts }
+    # end
   end
 
   # GET /microposts/1
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 # ActiveRecord is the default Rails module and Base is the base class for all models
 class User < ActiveRecord::Base
+  has_many :microposts
 end
